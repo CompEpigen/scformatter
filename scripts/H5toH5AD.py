db465c6632c907a486745cf0b7d821c55297be17
Mei-Ju May Chen <chenmjm@gmail.com>
2021-08-26 16:45:24 +0200

    standard sc analysis

--- scripts/H5toH5AD.py
+++ scripts/H5toH5AD.py
@@ -15,9 +15,30 @@ import re
 def getSyncLog(infoStr):
     os.system('echo "[%s] %s"' % (time.strftime('%H:%M:%S'), infoStr))
 
+sample = "rawdata/h5/GSM3489182_Donor_01_raw_gene_bc_matrices_h5.h5"
 def load_data(sample):
     adata = sc.read_10x_h5(sample)
     adata.var_names_make_unique()
+    sc.pp.filter_cells(adata, min_genes=200)
+    sc.pp.filter_genes(adata, min_cells=3)
+    adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
+    sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+    
+    adata = adata[adata.obs.n_genes_by_counts < 2500, :]
+    adata = adata[adata.obs.pct_counts_mt < 5, :]
+    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.log1p(adata)
+    sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+    
+    adata.raw = adata
+    adata = adata[:, adata.var.highly_variable]
+    sc.pp.regress_out(adata, ['total_counts', 'pct_counts_mt'])
+    sc.pp.scale(adata, max_value=10)
+    
+    sc.tl.pca(adata, svd_solver='arpack')
+    sc.pp.neighbors(adata, n_neighbors=10, n_pcs=40)
+    sc.tl.umap(adata)
+    sc.tl.leiden(adata)
     return adata
 
 def main():
